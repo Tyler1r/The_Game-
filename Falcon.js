19c28cd29013f1c329ad6cb71c9259ca140eb669
Tyler Rainey <tylerrainey5@gmail.com>
2021-08-17 11:12:31 -0400

    added app.js for movement object

---
+++ Falcon.js
@@ -0,0 +1,10 @@
+class Falcon {
+    constructor(){
+        this.spriteWidth = 250;
+        this.spriteHeight = 250;
+        this.width = this.spriteWidth/5;
+        this.height = this.spriteHeight/5;
+    }
+
+    
+}
