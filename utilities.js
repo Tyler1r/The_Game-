153ebbc8ec142f94d090c13da17ff312fe5c3841
Tyler Rainey <tylerrainey5@gmail.com>
2021-08-17 12:42:00 -0400

    added to utilities js

--- utilities.js
+++ utilities.js
@@ -1,3 +1,14 @@
 function animate(){
     ctx3.clearRect(0,0, canvas.width, canvas.height );
+    falcon.draw();
+    falcon.update();
+    requestAnimationFrame(animate);
 }
+
+animate();
+
+// to make object move you will need event listeners
+window.addEventListener('keydown', funtion(e){
+    keys = [];
+    keys.[e.keyCode = true;
+});
