26e7ff01d7ef41c04851b5e0ff6895a30ae55803
Tyler Rainey <tylerrainey5@gmail.com>
2021-08-17 10:32:28 -0400

    finished the setup.js

--- setup.js
+++ setup.js
@@ -1,3 +1,7 @@
+// using 5 Id's to organize the game 
+// 
+
+
 const canvas = document.getElementById('canvas1');
 const ctx1 = canvas.getContext('2d');
 canvas.width = 600;
@@ -28,3 +32,19 @@ const canvas = document.getElementById('canvas5');
 const ctx5 = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
+
+// global variables 
+const grid = 80;
+let keys =[];
+let score = 0 ;
+let collisionsCount = 0;
+let fraome = 0;
+let gameSpeed = 1;
+
+const particlesArray = [];
+const maxParticles = 300;
+const carsArray = [];
+//will contain tye fighters etc!
+const logsArray = [];
+// will contain things for M.Falcon to jump on 
+//
